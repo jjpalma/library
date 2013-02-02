3de2bd4fb45ce3aedf64db433f954337390bb509
Juan JosePalma Acosta <gepeto81@hotmail.com>
2013-02-02 10:51:12 -0500

    MVC

---
+++ Test/Case/Model/LoanTest.php
@@ -0,0 +1,42 @@
+<?php
+App::uses('Loan', 'Model');
+
+/**
+ * Loan Test Case
+ *
+ */
+class LoanTest extends CakeTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'app.loan',
+		'app.book',
+		'app.category'
+	);
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+		$this->Loan = ClassRegistry::init('Loan');
+	}
+
+/**
+ * tearDown method
+ *
+ * @return void
+ */
+	public function tearDown() {
+		unset($this->Loan);
+
+		parent::tearDown();
+	}
+
+}
